8705302bbe2c8b35eb651ba0a0aed1355e6d7477
vs801160-lab <vs801160@gmail.com>
2026-01-29 21:06:00 +0530

    Create Index.tsx

---
+++ Index.tsx
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/root';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root')!);
+root.render(<App />);
